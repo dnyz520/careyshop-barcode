e713f65ea8a52778294d8387ad0512077c4c8189
思维空间 <dnyz520@126.com>
2020-09-29 11:18:25 +0800

    no message

--- CodeItNow/BarCodeService.php
+++ CodeItNow/BarCodeService.php
@@ -6,14 +6,14 @@ use think\Config;
 use think\Route;
 use think\Service;
 
-class CodeItNowService extends Service
+class BarCodeService extends Service
 {
     public function boot()
     {
         $this->registerRoutes(function (Route $route) {
-            $route->get('info/codeitnow', function (Config $config) {
+            $route->get('info/barcode', function (Config $config) {
                 $data = $config->get('extra.product', []);
-                $data['conposer'] = 'CodeItNow';
+                $data['conposer'] = 'BarCode';
                 $data['statement'] = base64_decode('5pys56uZ5Z+65LqOQ2FyZXlTaG9w5ZWG5Z+O5qGG5p6257O757uf');
 
                 foreach ($data as $key => $value) {
